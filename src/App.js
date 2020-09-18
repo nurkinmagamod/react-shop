ac5512f4a412fa81f4e61e695c9213f2421b9519
nurkinmagamod <battchat@gmail.com>
2020-09-18 10:59:36 +0700

    Added menu-item navigation via withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage-component';
+
+import './App.scss';
+
+const HatsPage = props => {
+  console.log(props);
+  return(
+  <div>
+    <h1>HatsTitile {props.match.params.hatsId}</h1>
+  </div>
+  )
+  }
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats/:hatsId' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
