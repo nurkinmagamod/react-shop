a74f0a56835507a70f37b13956d3730b0be9e69d
nurkinmagamod <battchat@gmail.com>
2020-09-18 16:18:08 +0700

    Added shop page, collection prevew component, and collection item component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage-component';
-
+import ShopPage from './pages/shop/shop.component';
 import './App.scss';
-
-const HatsPage = props => {
-  console.log(props);
-  return(
-  <div>
-    <h1>HatsTitile {props.match.params.hatsId}</h1>
-  </div>
-  )
-  }
 
 function App() {
   return (
     <div >
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/hats/:hatsId' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
