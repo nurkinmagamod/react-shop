d8be31b799a9df7296579784ed24eaf068dbf897
nurkinmagamod <battchat@gmail.com>
2020-10-07 12:57:39 +0300

    Implemented userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+
 import './App.scss';
 
-class  App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
+class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        
-        userRef.onSnapshot(snapShot => {
-         this.setState({
-           currentUser:{
-             id: snapShot.id,
-             ...snapShot.data()
-           }
-         });
-          console.log(this.state);
-        });        
+
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
+        });
       }
-      this.setState({ currentUser: userAuth})
+      setCurrentUser(userAuth);
     });
   }
 
@@ -43,17 +38,20 @@ class  App extends React.Component {
 
   render() {
     return (
-      <div >
-        <Header currentUser={this.state.currentUser}/>
+      <div>
+        <Header />
         <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInSignUp}/>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInSignUp} />
         </Switch>
       </div>
-    );  
+    );
   }
-  
 }
 
-export default App;
+const mapDispachToProps = (dispach) => ({
+  setCurrentUser: (user) => dispach(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispachToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase, { app } from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase, { app } from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBlaWfL7VtelLJgf7LIYrHkRHZm6tmqkOo",
-  authDomain: "react-shop-31b0d.firebaseapp.com",
-  databaseURL: "https://react-shop-31b0d.firebaseio.com",
-  projectId: "react-shop-31b0d",
-  storageBucket: "react-shop-31b0d.appspot.com",
-  messagingSenderId: "638795862028",
-  appId: "1:638795862028:web:bac5d819c70abb28949e29",
+  apiKey: 'AIzaSyBlaWfL7VtelLJgf7LIYrHkRHZm6tmqkOo',
+  authDomain: 'react-shop-31b0d.firebaseapp.com',
+  databaseURL: 'https://react-shop-31b0d.firebaseio.com',
+  projectId: 'react-shop-31b0d',
+  storageBucket: 'react-shop-31b0d.appspot.com',
+  messagingSenderId: '638795862028',
+  appId: '1:638795862028:web:bac5d819c70abb28949e29',
 };
 
 export const createUserProfileDocument = async (userAuth, additionlData) => {
@@ -30,7 +30,7 @@ export const createUserProfileDocument = async (userAuth, additionlData) => {
         ...additionlData,
       });
     } catch (error) {
-      console.log("error create user", error.message);
+      console.log('error create user', error.message);
     }
   }
   return userRef;
@@ -42,7 +42,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
