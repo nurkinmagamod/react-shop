e0fff90cd54fdf4b61e40a8e532fb84964c681d3
nurkinmagamod <battchat@gmail.com>
2020-10-08 14:28:49 +0300

    Added cartIcon and cartDropdown components, implemented cart reducer

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -7,70 +7,68 @@ import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password:''
-        }
-    }
-    sayHi = () => {
-        console.log("say hi");
-    }
-    handleSubmit = async event => {
-            event.preventDefault();
-            console.log("Hi")
-            const { email, password } = this.state;
-
-            try {
-                await auth.signInWithEmailAndPassword(email, password);
-                this.setState({ email:'', password:''});
-            } catch(error){
-                console.log(error)
-            }
-
-            
+    this.state = {
+      email: '',
+      password: '',
     };
-    handleChange = event => {
-        const { value, name }  = event.target;
+  }
+  sayHi = () => {
+    console.log('say hi');
+  };
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Hi');
+    const { email, password } = this.state;
 
-        this.setState({ [name]: value })
-    } 
-    render() {
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                    name="email" 
-                    type="email" 
-                    handleChange={this.handleChange} 
-                    value={this.state.email}
-                    label='email' 
-                    required/>
-                    <FormInput 
-                    name="password" 
-                    type="password" 
-                    value={this.state.password} 
-                    handleChange={this.handleChange}
-                    label='password'
-                    required
-                    />
-                    <div className="buttons">
-                        <CustomButton onClick={this.handleSubmit} >Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
-                            {' '}
-                            Sign in with google{' '}
-                        </CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
+  };
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
+            <CustomButton onClick={this.handleSubmit}>Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {' '}
+              Sign in with google{' '}
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-
-export default SignIn; 
+export default SignIn;
