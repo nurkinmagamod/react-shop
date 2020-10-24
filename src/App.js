fe0270e7ace59ecc75a59155a7b0a2c1d3726726
nurkinmagamod <battchat@gmail.com>
2020-10-24 22:49:03 +0300

    Created checkout component and route for it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -43,6 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
