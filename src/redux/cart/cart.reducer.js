952c1dd6d134272fb454ae224bf41ffa7e5d477e
nurkinmagamod <battchat@gmail.com>
2020-11-01 23:49:39 +0300

    Added remove item from cart action

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,3 +1,4 @@
+import CartItem from '../../components/cart-item/cart-item.component';
 import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
 const INITIAL_STATE = {
@@ -16,6 +17,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
       return state;
