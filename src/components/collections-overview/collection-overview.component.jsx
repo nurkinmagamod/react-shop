246b31ad810b392cc19fe9f702bd0d5779f96aa0
nurkinmagamod <battchat@gmail.com>
2020-12-02 23:50:35 +0300

    Changed data representation and data obj

--- src/components/collections-overview/collection-overview.component.jsx
+++ src/components/collections-overview/collection-overview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/preview-collection/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
 
 import './collection-overview.styles.scss';
 
@@ -16,7 +16,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
